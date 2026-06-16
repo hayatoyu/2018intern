e83d1ebed1fdf2109708cf37fb8b88613ba456d0
Hayato <f19870421@gmail.com>
2026-06-16 17:14:53 +0800

    Implement to Exercise 2

---
+++ src/main/java/exercise1/Exercise1.java
@@ -0,0 +1,38 @@
+package exercise1;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 200;
+		Integer x1 = 200;
+		Integer x2 = 200;
+		float y = 3.5f;
+		Float y1 = 3.5f;
+		double z = 4.7;
+		Double z1 = 4.7;
+		char c = 'a';
+		Character c1 = 'a';
+		String s = "This is a string";
+		Object o1 = null;
+		Object o2 = new Object();
+		
+		System.out.println(x);
+		System.out.println(x1);
+		System.out.println(x1 == x2);
+		System.out.println(x1.equals(x2));
+		System.out.println(y);
+		System.out.println(y1);
+		System.out.println(y == y1);
+		System.out.println(z);
+		System.out.println(z1);
+		System.out.println(z == z1);
+		System.out.println(c);
+		System.out.println(c1);
+		System.out.println(c == c1);
+		System.out.println(s);
+		System.out.println(o1);
+		System.out.println(o2);
+	}
+
+}
---
+++ src/main/java/exercise2/Exercise2.java
@@ -0,0 +1,42 @@
+package exercise2;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 3;
+		int j = 5;
+		int a = 10, b = 8;
+		double c = 2.5, d = 0.5;
+		System.out.println("========== i ==========");
+		System.out.println(i);
+		System.out.println(i++);
+		System.out.println(i);
+		
+		System.out.println("========== j ==========");
+		System.out.println(j);
+		System.out.println(++j);
+		System.out.println(j);
+		
+		System.out.println("========== a + b ==========");
+		System.out.println(a + b);
+		
+		System.out.println("========== c * d ==========");
+		System.out.println(c * d);
+		
+		System.out.println("========== a / b ==========");
+		System.out.println(a / b);
+		
+		System.out.println("========== c / d ==========");
+		System.out.println(c / d);
+		
+		System.out.println("========== j % i ==========");
+		System.out.println(j % i);
+		
+		System.out.println("========== i % j ==========");
+		System.out.println(i % j);
+		
+		
+	}
+
+}
